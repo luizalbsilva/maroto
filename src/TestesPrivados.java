33ff1234d81a8eaac62f6d356a6204984b3d2913
Luiz Alberoni da Silva <luizalbsilva@gmail.com>
2012-11-24 12:00:52 -0200

    Inicio do projeto

---
+++ src/TestesPrivados.java
@@ -0,0 +1,14 @@
+
+public class TestesPrivados {
+	private String contadorNaoUtilizado;
+	
+	private void metodoNaoInvocado() {
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+}
